51b81f6f6334146bbde4f90b1df365f0efb0473d
progre <djyayutto@gmail.com>
2013-12-28 14:55:51 +0900

    rebase

--- src/public/javascript/main.ts
+++ src/public/javascript/main.ts
@@ -1,3 +1,22 @@
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
 /// <reference path="../../typings/linq.d.ts"/>
 /// <reference path="../../typings/promise.d.ts"/>
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
+/// <reference path="../../typings/angularjs/angular-route.d.ts"/>
+
+var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+app.config(['$routeProvider',
+    ($routeProvider: ng.route.IRouteProvider) => {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'html/index.html', controller: 'IndexController'
+            }).otherwise({
+                templateUrl: 'html/404.html'
+            });
+    }
+]);
+app.controller('IndexController',
+    [() => {
+    }]);
+
+angular.bootstrap(<any>document, ['app']);
