55ce3c642712d0c9e481c56c3e3eff8a2799b85e
progre <djyayutto@gmail.com>
2014-02-23 01:08:22 +0900

    fix: promise

--- src/typings/promise.d.ts
+++ src/typings/promise.d.ts
@@ -1,12 +1,14 @@
-declare class Promise {
-    static cast(obj: any): Promise;
-    static resolve(obj: any): Promise;
-    static reject(obj: any): Promise;
+declare class Promise<T> {
+    static cast<T>(obj?: T): Promise<T>;
+    static resolve(obj: any): Promise<any>;
+    static reject(obj: any): Promise<any>;
     static all(array: any[]): any;
     static race(array: any[]): any;
 
-    constructor(func: (resolve: Function, reject: Function) => void);
+    constructor(func: (resolve: (aValue: T) => void, reject: (aReason: any) => void) => void);
 
-    then(onFulfilled: Function, onRejected?: Function);
-    catch(onRejected: Function);
+    then<U>(onFulfilled: (val: T) => Promise<Promise<U>>, onRejected?: Function): Promise<U>;
+    then<U>(onFulfilled: (val: T) => Promise<U>, onRejected?: Function): Promise<U>;
+    then<U>(onFulfilled: (val: T) => U, onRejected?: Function): Promise<U>;
+    catch(onRejected: Function): any;
 }
