85fbff9cf7d6dac4aac1aaa1095963791b07acaa
progre <djyayutto@gmail.com>
2014-02-23 22:01:29 +0900

    fix: resolve

--- src/typings/promise.d.ts
+++ src/typings/promise.d.ts
@@ -5,7 +5,7 @@ declare class Promise<T> {
     static all(array: any[]): any;
     static race(array: any[]): any;
 
-    constructor(func: (resolve: (aValue: T) => void, reject: (aReason: any) => void) => void);
+    constructor(func: (resolve: (aValue?: T) => void, reject: (aReason?: any) => void) => void);
 
     then<U>(onFulfilled: (val: T) => Promise<Promise<U>>, onRejected?: Function): Promise<U>;
     then<U>(onFulfilled: (val: T) => Promise<U>, onRejected?: Function): Promise<U>;
